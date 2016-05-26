4da3114cb9350da5254dfa73606b136d49f36f3b
Eugen <e.lozovyi@gmail.com>
2016-05-26 20:19:39 +0300

    changed

--- goit/dir/tabs/js/script.js
+++ goit/dir/tabs/js/script.js
@@ -1,11 +1,41 @@
+// Tabs
 $(function() {
 
   var $caption = $('ul.tabs__caption');
-  var $tabs = $('p');
+  var $content = $('div.tabs__content p');
 
   $caption.on('click', 'li:not(.active)', function() {
-    $(this).addClass('active').siblings().removeClass('active');
-    $(this).closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content')
+      .removeClass('active').eq($(this).index()).addClass('active')
+      .parent().find('div.tabs__content p').effect( "slide", "slow" );
+  });
+});
 
+// Tooltip
+$(function() {
+
+  var $tooltip = $('fieldset div');
+  var $helpButton = $('button');
+
+  $tooltip.mouseover(function(){
+    $(this).children('span').show('slow');
+
+  }).mouseout(function(){
+    $(this).children('span').hide('slow');
+  });
+
+  $helpButton.on('click', function(e) {
+    $tooltip.find('span').each(function(){
+      $(this).show(700,'swing');
+
+      var $el = $(this);
+      setTimeout(function(){
+        $el.hide(1000, 'swing');
+    }, 2000);
     });
+
+    e.preventDefault();
+  });
 });
