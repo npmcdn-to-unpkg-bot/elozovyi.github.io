19edd454c5946a4f5f84b5ccf5a8ce47f005b35a
Eugen <e.lozovyi@gmail.com>
2016-06-26 13:41:46 +0300

    commit

--- goit/javascript/homework15-16/js/script.js
+++ goit/javascript/homework15-16/js/script.js
@@ -1,26 +1,120 @@
-$(function(){
-	$('.google_search').submit(function(){
+// OOP
 
-		var APIurl = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='
-						+ encodeURIComponent($('#settings').val()) + '&callback=GoogleCallback&context=?';
+function Human(){
 
-		$.ajax ({
-			url: APIurl,
-			dataType: 'jsonp',
-			success: function(data, textStatus){
-				var ul = document.createElement("ul");
-				$.each(data.results, function(i, val){
-					var li = document.createElement("li");
-					li.innerHTML = '<a href="'+val.url+'"target="_blank">'+val.title+'</a><p class="url">'+val.url+'</p><p class="content">'+val.content+'</p>';
-					ul.appendChild(li);
-				});
-				$('.google_search__result').html(ul);
-			}
-		});
-		return false;
-	});
+  this.name = 'Ivan';
+  this.age = 18;
+  this.sex = 'male';
+  this.height = 1.82;
+  this.weight = 65;
+}
+
+function Worker(placeOfWork){
+  this.placeOfWork = placeOfWork;
+  this.zp = 2000;
+  this.work = function(){
+    console.log('I am working');
+
+  };
+
+}
+
+function Student() {
+  this.placeOfStudy = 'Univer';
+  this.grant = 100;
+  this.action = function(){
+    console.log('watch TV');
+
+  };
+
+}
+
+var human1 = new Human();
+
+Student.prototype = human1;
+Worker.prototype = human1;
+
+var worker1 = new Worker('city');
+var worker2 = new Worker('village');
+var student1 = new Student();
+var student2 = new Student();
+
+console.log('Worker 1 age = ' + worker1.age);
+console.log('Worker 1 place = ' + worker1.placeOfWork);
+console.log('Worker 2 age = ' + worker2.age);
+console.log('Worker 2 place = ' + worker2.placeOfWork);
+worker1.work();
+
+console.log('Student 1 name = ' + student1.name);
+console.log('Student 1 age = ' + student2.age);
+student1.action();
+
+
+// API
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
 });
 
-function GoogleCallback (func, data) {
-    window[func](data);
+//Second call with this:
+// Get object of URL parameters
+var allVars = $.getUrlVars();
+
+// Getting URL var by its nam
+var keyword = $.getUrlVar('s');
+
+if (keyword) {
+    $('.search_input').val(keyword);
+    search();
 }
+
+function search() {
+    var params = {
+        // Request parameters
+        "tag": $('.search_input').val(),
+        "limit": "10",
+        "key": "LIVDSRZULELA"
+    };
+
+    $.ajax({
+        url: "https://api.riffsy.com/v1/search?" + $.param(params),
+        type: "GET"
+    })
+    .done(function(data) {
+        console.log(data);
+        var results = data.results,
+            imageContainer = $('.wrapper-image');
+        imageContainer.empty();   
+        results.forEach(function(result) {
+            imageContainer.append('<img src="' + result.url + '">');
+        })
+    })
+    .fail(function() {
+        alert("error");
+    });
+}
+
+$(function() {
+    $('.search_btn').click(search);
+    $('.search_input').keyup(function(e) {
+        var code = e.which;
+        if (code == 13) {
+            e.preventDefault();
+            search();
+        }
+    });
+
+
+});
