6b9a8f2c68a1381c34aa7dc0a130d1885992679a
Eugen <e.lozovyi@gmail.com>
2016-06-27 19:26:11 +0300

    commit

--- goit/javascript/homework17-18/js/script.main.js
+++ goit/javascript/homework17-18/js/script.main.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.script2').innerHTML = "This is script2";
 });
+;"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.script1').innerHTML = "This is script1";
+});
+;"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.script2').innerHTML = "This is script2";
+});
+;"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.script1').innerHTML = "This is script1";
+});
+;"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.script2').innerHTML = "This is script2";
+});
