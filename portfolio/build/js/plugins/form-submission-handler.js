26cbb7b8aa61fec458025f65ae2526218de86b6d
Eugen <e.lozovyi@gmail.com>
2016-08-02 16:56:45 +0300

    changed

--- portfolio/build/js/plugins/form-submission-handler.js
+++ portfolio/build/js/plugins/form-submission-handler.js
@@ -36,7 +36,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.onreadystatechange = function() {
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
-        document.getElementById('modal').style.display = 'none'; // hide form
+        document.getElementById('gform').style.display = 'none'; // hide form
         document.getElementById('thankyou_message').style.display = 'block';
         return;
     };
