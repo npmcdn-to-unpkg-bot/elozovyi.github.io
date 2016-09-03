6eb45d029ab9449e9901f08f49543c8867820309
Eugen Lozovyi <e.lozovyi@gmail.com>
2016-09-03 03:21:02 +0300

    tablet & desktop version made

--- examples/astound_commerce/src/js/main.js
+++ examples/astound_commerce/src/js/main.js
@@ -8,12 +8,18 @@ var modalContent = document.getElementsByClassName("modal__content")[0];
 
 // Get the button that opens the modal
 var btn = document.getElementById("popup-btn");
+var btn2 = document.getElementById("popup-btn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+    modal.style.display = "block";
+    modalContent.style.display = "block";
+}
+
+btn2.onclick = function() {
     modal.style.display = "block";
     modalContent.style.display = "block";
 }
